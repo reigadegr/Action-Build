00152f792e853e73da928cd3eaac9ea2e8db5e4a
reigadegr <272268642@qq.com>
2025-05-19 20:37:39 +0000

    进阶测试

--- template.c
+++ template.c
@@ -4,13 +4,17 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("DeepSeek");
 MODULE_DESCRIPTION("Dummy module to block oplus_network_game_first");
 MODULE_ALIAS("oplus_network_game_first");
+MODULE_INFO(vermagic, "6.6.30-android15-8-o-4k SMP preempt mod_unload modversions aarch64");
 
-static int __init dummy_init(void) {
+struct module __this_module;
+EXPORT_SYMBOL(__this_module);
+
+static int __init init_module(void) {
     return 0;
 }
 
-static void __exit dummy_exit(void) {
+static void __exit cleanup_module(void) {
 }
 
-module_init(dummy_init);
-module_exit(dummy_exit);
+module_init(init_module);
+module_exit(cleanup_module);
