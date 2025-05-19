8101ed17ebb8a6056ed9ff6bd651381515c50cc1
reigadegr <272268642@qq.com>
2025-05-19 21:19:12 +0000

    fix

--- template.c
+++ template.c
@@ -9,12 +9,12 @@ MODULE_INFO(vermagic, "6.6.30-android15-8-o-4k SMP preempt mod_unload modversion
 struct module __this_module;
 EXPORT_SYMBOL(__this_module);
 
-static int __init init_module(void) {
+static int __init dummy_init(void) {
     return 0;
 }
 
-static void __exit cleanup_module(void) {
+static void __exit dummy_exit(void) {
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(dummy_init);
+module_exit(dummy_exit);
